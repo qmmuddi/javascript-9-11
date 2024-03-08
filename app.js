a335d01641500c4b700cfbd08c1ef273e84ee86d
Muhammad Muddassir <155112096+qmmuddi@users.noreply.github.com>
2024-03-08 07:26:49 +0500

    new

---
+++ app.js
@@ -0,0 +1,34 @@
+var user_input =prompt("what is your city name?");
+if("user_input===karachi"){
+   console.log("welcometo to the city of the lights");
+}
+var  user_input1 =prompt("enter color");
+if("user_input1===red"){
+    console.log("must stop");
+}
+else if("user_input1===yellow"){
+    console.log("ready to go");
+}
+else if("user_input1===green"){
+    console.log("move");
+}
+else{
+    console.log("color not valid");
+}
+var user_input2 =prompt("what is your gender?");
+if("user_input2===male"){
+    console.log("good morning sir");
+}
+else if("user_input2===female"){
+    console.log("good morning sir");
+}
+ else if("user_input2===another"){
+    console.log("good morning");
+}
+else{
+    console.log("gender not valid");
+}
+var b = 82;
+if (b++ === 83){
+alert("given condition for variable b is true");
+}
